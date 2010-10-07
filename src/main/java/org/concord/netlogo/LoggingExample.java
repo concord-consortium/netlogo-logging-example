79b573eafe75947451032561d15136fd6d954199
Aaron Unger <aunger@concord.org>
2010-10-07 14:00:19 -0400

    Netlogo+Webstart globals logging problem demo initial import.

---
+++ src/main/java/org/concord/netlogo/LoggingExample.java
@@ -0,0 +1,156 @@
+package org.concord.netlogo;
+
+import java.awt.EventQueue;
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.swing.JFrame;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.spi.LoggingEvent;
+import org.apache.log4j.varia.NullAppender;
+import org.nlogo.api.CompilerException;
+import org.nlogo.lite.InterfaceComponent;
+import org.nlogo.log.LogMessage;
+import org.nlogo.window.InvalidVersionException;
+import org.nlogo.workspace.AbstractWorkspace;
+
+public class LoggingExample {
+    private static InterfaceComponent app;
+
+    public static void main(String[] args) {
+        final JFrame mainFrame = new JFrame("NetLogo Logging Example");
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainFrame.pack();
+        mainFrame.setVisible(true);
+        
+        AbstractWorkspace.isApp(true);
+        AbstractWorkspace.isApplet(true);
+
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    app = new InterfaceComponent(mainFrame);
+                    
+                    mainFrame.add(app);
+                    
+                    setupLogging();
+                    
+                    loadModel(LoggingExample.class.getResource("model.nlogo"));
+                    
+                    doCommand("mysetup");
+                } catch (InvalidVersionException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                
+                mainFrame.pack();
+                mainFrame.setVisible(true);
+            }
+        });
+        
+    }
+
+    private static void setupLogging() {
+        Reader reader = getLoggerProperties();
+        app.startLogging(reader, "user");
+
+        Logger variablesLogger = Logger.getLogger("org.nlogo.log.Logger");
+        variablesLogger.setLevel(Level.INFO);
+        EventAppender eventAppender = new EventAppender();
+        variablesLogger.addAppender(eventAppender);
+    }
+    
+    private static void loadModel(URL modelLocation) throws InvalidVersionException, IOException {
+        app.setPrefix(modelLocation);
+        String source = getStringFromUrl(modelLocation);
+        app.openFromSource("Model", null, source);
+    }
+    
+    private static String getStringFromUrl(URL url) throws IOException {
+        InputStream inputStream = url.openStream();
+        ByteArrayOutputStream bout = new ByteArrayOutputStream();
+        byte [] buffer = new byte[1024];
+        int len;
+        while((len = inputStream.read(buffer)) > 0) {
+            bout.write(buffer, 0, len);
+        }
+        return bout.toString();
+    }
+    
+    private static Reader getLoggerProperties(){
+        BufferedReader br = null;
+
+        URL propertiesFile = LoggingExample.class.getResource("netlogo_logging.xml");
+        if (propertiesFile != null){
+            try {
+                br = new BufferedReader(new InputStreamReader(propertiesFile.openStream()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return br;
+    }
+    
+    private static void doCommand(String command)
+    {
+        try {
+            app.commandLater(command);
+        } catch (CompilerException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
+
+/**
+ * The EventAppender listens for every invocation of doAppend
+ * by the Logger. The LogMessage that comes back can then
+ * be parsed for information about the variable that has
+ * changed and its new value.
+ * 
+ * Each log message has an array of attributes (e.g. "event" and
+ * a type), and an array of elements, which consist of variable 
+ * or button names and new values or actions.
+ * 
+ * Sample sets of attributes and elements:
+ * 
+ * [event type:globals, elements:[[tag:name, data:MY-RABBIT-ENERGY], [tag:value, data:5.0]] ] 
+ * [event type:button, elements:[[tag:name, data:setup], [tag:action, data:released], [tag:releaseType, data:once]] ]
+ * [event type:button, elements:[[tag:name, date:go], [tag:action, data:pressed], [tag:releaseType, data:user]] ]
+ *
+ */
+class EventAppender extends NullAppender {
+    @Override
+    public void doAppend(LoggingEvent event) {
+        LogMessage logMessage = (LogMessage) event.getMessage();
+        
+        HashMap<String, String> info = getEventInfo(logMessage);
+        
+        debugMessage(info);
+    }
+    
+    private HashMap<String, String> getEventInfo(LogMessage message) {
+        HashMap<String, String> info = new HashMap<String, String>();
+        for (LogMessage element : message.elements) {
+            info.put(element.tag, element.data);
+        }
+        return info;
+    }
+    
+    private void debugMessage(HashMap<String, String> info) {
+      System.out.println("\n----------");
+      for (Map.Entry<String, String> e : info.entrySet()) {
+          System.out.println(e.getKey() + ": " + e.getValue());
+      }
+    }
+}
